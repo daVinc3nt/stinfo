2fbb91c1ac1a289c5d436f887a560894cf75e649
AtomsOne <thinh.nguyen1412bkcs90@hcmut.edu.vn>
2024-05-06 18:44:39 +0700

    chinh sua trang thong tin sv

--- components/MyBiography/content/MainInfo.tsx
+++ components/MyBiography/content/MainInfo.tsx
@@ -55,13 +55,13 @@ const MainInfo = () => {
                             <div className="font-bold  items-start">Họ và tên:</div>
                             <div className="items-center md:mx-px sm:px-4 px-10">{studentData.fullname}</div>
                         </div>
-                        <div className="flex justify-start">
-                            <div className="font-bold  items-start">MSSV:</div>
+                        <div className="flex justify-start sm:pr-[38.5%] ">
+                            <div className="font-bold  items-start ">MSSV:</div>
                             <div className="items-center md:mx-px sm:px-4 px-10">{studentData.student_id}</div>
                         </div>
                     </div>
 
-                    <div className="md:flex justify-start">
+                    <div className="flex justify-start">
                             <div className="font-bold  items-start">Ngày tháng năm sinh:</div>
                             <div className="items-center md:mx-px sm:px-4 px-10">{studentData.date_of_birth}</div>
                     </div>
@@ -84,46 +84,40 @@ const MainInfo = () => {
         <div className="space-y-6">
             <div className="flex items-center justify-center w-full h-[42px]
                              text-white text-2xl font-bold bg-sky-500 rounded-xl">Thông tin học vụ</div>
-            <div className="border border-gray-200 p-4 rounded-xl mb-4">
-                <div className="space-y-3 pl-10 text-lg">
-                    <div className="xl:flex justify-between">
+            <div className="border border-gray-200 p-4 rounded-xl mb-4 ">
+                <div className="sm:grid sm:grid-cols-2 space-y-3 pl-10 text-lg">
+                    <div className="sm:col-span-1 m-2 space-y-4">
                         <div className="md:flex justify-start">
                             <div className="font-bold  items-start">Khoa quản lý:</div>
                             <div className="items-center md:mx-px sm:px-4 px-10">{studentData.faculty}</div>
                         </div>
-                        <div className="flex justify-start">
-                            <div className="font-bold  items-start">Tình trạng:</div>
-                            <div className="items-center md:mx-px sm:px-4 px-10">{studentData.active ==1 ? "Đang học" : "Tạm dừng"}</div>
-                        </div>
-                    </div>
-
-            
-                    <div className="xl:flex justify-between">
-                        <div className="flex justify-start">
+                        <div className="md:flex justify-start ">
                             <div className="font-bold  items-start">Ngành đào tạo:</div>
                             <div className="items-center md:mx-px sm:px-4 px-10">{studentData.major}</div>
                         </div>
                         <div className="flex justify-start">
-                            <div className="font-bold  items-start">Chương trình đào tạo:</div>
-                            <div className="items-center md:mx-px sm:px-4 px-10">{studentData.program}</div>
-                        </div>
-                    </div>
-
-                    <div className="xl:flex justify-between">
-                        <div className="flex justify-start">
                             <div className="font-bold  items-start">Lớp:</div>
                             <div className="items-center md:mx-px sm:px-4 px-10">{studentData.class}</div>
                         </div>
-                        <div className="flex justify-start">
-                            <div className="font-bold  items-start">trình độ:</div>
+                        <div className="md:flex justify-start">
+                            <div className="font-bold  items-start">Thời gian đào tạo tối đa:</div>
+                            <div className="items-center md:mx-px sm:px-4 px-10">{studentData.exclusion}</div>
+                        </div> 
+                    </div>
+                    <div className="col-span-1 m-2 space-y-4">
+                        <div className="flex justify-start ">
+                            <div className="font-bold  items-start">Tình trạng:</div>
+                            <div className="items-center md:mx-px sm:px-4 px-10">{studentData.active ==1 ? "Đang học" : "Tạm dừng"}</div>
+                        </div>
+                        <div className="flex justify-start ">
+                            <div className="font-bold  items-start">Chương trình đào tạo:</div>
+                            <div className="items-center md:mx-px sm:px-4 px-10">{studentData.program}</div>
+                        </div>
+                        <div className="flex justify-start ">
+                            <div className="font-bold  items-start">Trình độ:</div>
                             <div className="items-center md:mx-px sm:px-4 px-10">{studentData.level}</div>
                         </div>
                     </div>
-
-                    {/* <div className="md:flex justify-start">
-                            <div className="font-bold  items-start">Thời gian đào tạo tối đa:</div>
-                            <div className="items-center md:mx-px sm:px-4 px-10">{student.study.Timeline}</div>
-                    </div> */}
                 </div>    
             </div>
             
@@ -141,17 +135,17 @@ const MainInfo = () => {
                     <div className="w-[98%] h-[42%] px-2 py-6 sm:p-10 
                                     border rounded-lg shadow-sm shadow-teal-200">
                             <div className="space-y-3 text-lg">
-                                <div className="sm:flex justify-start border-solid">
+                                <div className="flex justify-start border-solid sm:pr-[30%]">
                                     <div className="font-bold  items-start">CCCD/CMND:</div>
-                                    <div className="items-center sm:mx-auto sm:px-4 px-10">{studentData.credential_id}</div>
+                                    <div className="items-center sm:mx-auto sm:px-4 px-5">{studentData.credential_id}</div>
                                 </div>
                                 {/* <div className="md:flex justify-start border-solid">
                                     <div className="font-bold  items-start">Nơi sinh:</div>
                                     <div className="items-center sm:mx-auto sm:px-4 px-10">{}</div>
                                 </div> */}
-                                <div className="flex justify-start border-solid">
+                                <div className="flex justify-start border-solid sm:pr-[34%]">
                                     <div className="font-bold  items-start">Hộ khẩu thường trú:</div>
-                                    <div className="items-center sm:mx-auto sm:px-4 px-10">{studentData.address}</div>
+                                    <div className="items-center sm:mx-auto sm:px-4 px-5">{studentData.address}</div>
                                 </div>
                             </div>
                     </div>
