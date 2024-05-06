b6e770116ac9353f4460cc8ef1e340da86e8a3d5
Nguyễn Thành Danh <nguyenthanhdanh23072004@gmail.com>
2024-05-06 09:19:31 +0700

    update homework

--- components/Homework/SubmissionComponent/SubmissionField.tsx
+++ components/Homework/SubmissionComponent/SubmissionField.tsx
@@ -125,11 +125,28 @@ const SubmissionField = (props: { role: string }) => {
 		};
 
 		return (
-			<div className="py-3 flex items-center">
+			<div className="py-3">
+				 {/* the input element actually upload the file, but hidden from the UI, the button will 
+				 trigger the onChange event */}
+				<input type='file' id="selected_file" className=' hidden' 
+					onChange={(event) => {
+					// handle file change and upload file
+					if (event.target.files && event.target.files.length > 0) {
+						const newFile : SubmitFile = {
+							submitFile : event.target.files[0]
+						}
+						setFile(newFile);
+						// props.setNumFile(props.numFile + 1);
+					}
+				}}
+					onClick={(event) => {
+						// reset the input element value to allow the same file to be uploaded
+						event.currentTarget.value = '';
+				}}>
+				</input>
 				<button
-					className="bg-blue-500 text-center py-3 px-4 text-white border rounded-md hover:bg-blue-800"
-					onClick={handleClick}
-				>
+					className="bg-blue-500 text-center block py-3 px-4 text-white border rounded-md hover:bg-blue-800" 
+					onClick={handleClick}>
 					Thêm bài nộp
 				</button>
 				{
